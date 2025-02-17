c4d46a7c4073078f776731f58d952fd7774064d1
Davy SAINT-MARTIN <davy.saintmartin@gmail.com>
2025-02-17 15:56:30 +0100

    Ajout variables

---
+++ Infrastructure/Terraform/variables.tf
@@ -0,0 +1,24 @@
+variable "team_name" {
+  description = "The name of the team"
+  type        = string
+}
+
+variable "player1" {
+  description = "The name of player 1"
+  type        = string
+}
+
+variable "player2" {
+  description = "The name of player 2"
+  type        = string
+}
+
+variable "player3" {
+  description = "The name of player 3"
+  type        = string
+}
+
+variable "player4" {
+  description = "The name of player 4"
+  type        = string
+}
